44e772819edb523ce13e6dd6578536668ba5ac69
Eduardo Evangelista <eduardo.evangelista2002@gmail.com>
2024-08-07 11:42:38 -0300

    :bug: fix: + y in whells

--- base.py
+++ base.py
@@ -6,18 +6,17 @@ import utils
 
 def draw_base():
     # DRAW WHEEL 1
+    
     glPushMatrix()
-    reimplementation.glTranslatef(0, -2, -6)
+    reimplementation.glTranslatef(0, -1.5, -6)
     reimplementation.glScalef(0.2, 0.2, 0.2)
-    glColor3f(1, 0, 0)
     glutSolidTorus(0.5, 1, 30, 30)
     glPopMatrix()
 
     # DRAW WHEEL 2
     glPushMatrix()
-    reimplementation.glTranslatef(0, -2, -4)
+    reimplementation.glTranslatef(0, -1.5, -4)
     reimplementation.glScalef(0.2, 0.2, -0.2)
-    glColor3f(0, 1, 0)
     glutSolidTorus(0.5, 1, 30, 30)
     glPopMatrix()
 
